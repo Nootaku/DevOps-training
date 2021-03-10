607b61cfea1f93718f14c5bda8a6fad60a5320a1
Max WATTEZ <max.wattez@protonmail.com>
2021-03-10 10:30:14 +0100

    HTML template

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ Latest update: 10/03/2021
 """
 
 # Import Flask
-from flask import Flask
+from flask import (
+    Flask,           # default flask class
+    render_template  # allows us to create HTML templates
+)
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -15,9 +18,11 @@ app = Flask(__name__)
 # hello_world function on that route
 @app.route('/')
 def hello_world():
-    """Simple hello_world.
+    """Uses the render_template function to render the file "home.html".
+    All templates must be located in a folder called 'template' in the root of
+    the project.
     """
-    return 'Hello, World!'
+    return render_template("home.html")
 
 
 # As this is the main file of our minimal application, when called the service
